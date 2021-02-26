e94948b77a637d43db26d9413a382c66d48ff7da
gbrabosa <gbrabosa@gmail.com>
2021-02-25 21:27:23 -0300

    Grava arquivo de configuração

--- arduino/irrigador_automatico/irrigador_automatico.ino
+++ arduino/irrigador_automatico/irrigador_automatico.ino
@@ -1,9 +1,12 @@
 #include <SD.h>
+#include <SPI.h>
 #define ENTRADA_SENSOR A0 //Pino usado para a leitura do sensor
 #define VALOR_SOLO_MOLHADO 768
 #define VALOR_SOLO_SECO 256
 
 #define ACIONAMENTO_MOTOR 9
+
+File myFile;
 
 float valorSensor = 0;
 bool motorLigado = false;
@@ -51,11 +54,50 @@ void modificaAcionamentoMotor(bool valor){
   motorLigado = valor;
   digitalWrite(ACIONAMENTO_MOTOR, valor ? HIGH : LOW);
 }
-void setup() {
-  Serial.begin(9600);
-  pinMode(ACIONAMENTO_MOTOR, OUTPUT);
+
+
+void inicializaConfig(){
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  // if the file opened okay, write to it:
+  if (SD.exists("config.txt")) {
+    Serial.print("Arquivo de configuracao encontrado.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("Arquivo de configuracao nao encontrado.");
+    Serial.println("Criando arquivo de configuração");
+    myFile = SD.open("config.txt", FILE_WRITE);
+    myFile.println("400");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  }  
+  // Open the file for reading:
+  myFile = SD.open("config.txt");
+  if (myFile) {
+    Serial.println("config.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  }
 }
 
+
+void setup() {
+  Serial.begin(9600);
+  inicializaConfig();
+  pinMode(ACIONAMENTO_MOTOR, OUTPUT);
+}
 void loop() {
   delay(1000);
   modificaAcionamentoMotor(!motorLigado);
