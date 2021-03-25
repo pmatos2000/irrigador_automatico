446719f7254b7f56e0ab9901480479da0cfdb962
Paulo <pmatos2000@gmail.com>
2021-03-25 17:27:21 -0300

    data

--- android/irrigador_automatico/app/src/main/java/br/labmult/irrigadorautomatico/ui/configuracao/ConfiguracaoFragment.kt
+++ android/irrigador_automatico/app/src/main/java/br/labmult/irrigadorautomatico/ui/configuracao/ConfiguracaoFragment.kt
@@ -1,5 +1,6 @@
 package br.labmult.irrigadorautomatico.ui.configuracao
 
+import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -26,7 +27,9 @@ class ConfiguracaoFragment : Fragment() {
                 ViewModelProvider(this).get(ConfiguracaoViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_configuracao, container, false)
         val textHoraAtual = root.findViewById<TextView>(R.id.textHoraAtual);
+        val textDataAtual = root.findViewById<TextView>(R.id.textDataAtual)
         val btnAjustarHora = root.findViewById<Button>(R.id.btnAjustarHora);
+        val btnAjustarData = root.findViewById<Button>(R.id.btnAjustarData);
         val cal = Calendar.getInstance()
         btnAjustarHora.setOnClickListener {
             val seletorHoras = TimePickerDialog.OnTimeSetListener{ timePicker, hora, min ->
@@ -35,6 +38,15 @@ class ConfiguracaoFragment : Fragment() {
                 textHoraAtual.text = SimpleDateFormat("HH:mm").format(cal.time);
             }
             TimePickerDialog(context, seletorHoras, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+        btnAjustarData.setOnClickListener {
+            val seletorData = DatePickerDialog.OnDateSetListener { dataPicker, ano, mes, dia ->
+                cal.set(Calendar.YEAR, ano)
+                cal.set(Calendar.MONTH, mes)
+                cal.set(Calendar.DAY_OF_MONTH, dia)
+                textDataAtual.text = SimpleDateFormat("dd/MM/YYYY").format(cal.time)
+            }
+            activity?.let { it1 -> DatePickerDialog(it1, seletorData, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show() }
         }
         return root
     }
